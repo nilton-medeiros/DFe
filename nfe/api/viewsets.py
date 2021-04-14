8d856f85dd2e38425bdd772e41d857917d795e8b
nilton-medeiros <nilton@sistrom.com.br>
2021-04-14 19:23:09 -0300

    Adicionado a lib PyNFe e trabalhando na nfe.api.viewsets.py

--- nfe/api/viewsets.py
+++ nfe/api/viewsets.py
@@ -3,14 +3,38 @@ from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 
 
+def enviar_nfe(data, ref):
+    if type(data) == dict:
+        # processar json-dict
+        respond = {
+            "http_code": 202,
+            "cnpj_emitente": "07504505000132",
+            "ref": ref,
+            "status": "processando_autorizacao",
+            "mensagem": "Processando autorização"
+        }
+    else:
+        respond = {
+            "http_code": 400,
+            "codigo": "json_parse_error",
+            "mensagem": "Bad Request - O texto não é um formato json válido"
+        }
+
+    return respond
+
+
 class NfeViewSet(APIView):
     permission_classes = (AllowAny,)
 
+    # Recebe o JSON da NFe, gera o XML, Assina, Valida e envia à Sefaz se não houver erro
     def post(self, request):
-        json_nfe = request.data
-        print(json_nfe)
-        print(type(json_nfe))
-        response = {
-            "sucesso": True
-        }
-        return Response(response, status=200)
+        json_file = request.data
+        if 'ref' in request.headers:
+            refer = request.headers['ref']
+        else:
+            refer = None
+        response = enviar_nfe(data=json_file, ref=refer)
+        # print(response)
+        http_code = response['http_code']
+        del response['http_code']
+        return Response(response, status=http_code)
