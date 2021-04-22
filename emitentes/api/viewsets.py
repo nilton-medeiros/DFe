8c89ff520c69e5b77b8b25a7b921d42a31957e32
Regis Santos <regis42santos@gmail.com>
2021-04-22 18:19:47 -0300

    Retorna somente os dados do usuário logado.

--- emitentes/api/viewsets.py
+++ emitentes/api/viewsets.py
@@ -1,11 +1,24 @@
 from rest_framework import viewsets
+from rest_framework.authentication import BasicAuthentication, TokenAuthentication
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
 
 from emitentes.api import serializers
 from emitentes import models
 
 
 class EmitenteViewSet(viewsets.ModelViewSet):
-    permission_classes = (AllowAny,)
     serializer_class = serializers.EmitenteSerializer
     queryset = models.Emitente.objects.all()
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (BasicAuthentication, TokenAuthentication)
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+
+        user = request.user
+        if user.is_authenticated:
+            queryset = queryset.filter(user_id=user)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
