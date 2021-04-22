9d0f0edd76bbbc53dfd586f737ae8ab7e8794a5c
Regis Santos <regis42santos@gmail.com>
2021-04-22 17:54:25 -0300

    Retorna os dados no serializer com condicionais.

--- emitentes/api/serializers.py
+++ emitentes/api/serializers.py
@@ -5,27 +5,29 @@ from emitentes import models
 class EmitenteSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Emitente
-        tup1 = ('id', 'nome')
+        fields = ('id', 'nome')
 
-        if model.cnpj:
-            tup2 = ('cnpj',)
+        # if model.nfe_emite:
+        #     tup3 = ('nfe_serie', 'nfe_numero', 'nfe_serie_homologacao', 'nfe_numero_homologacao')
+        # if model.nfce_emite:
+        #     tup4 = ('nfce_serie', 'nfce_numero', 'nfce_serie_homologacao', 'nfce_numero_homologacao')
+        # if model.cte_emite:
+        #     tup5 = ('cte_serie', 'cte_numero', 'cte_serie_homologacao', 'cte_numero_homologacao')
+        # if model.mdfe_emite:
+        #     tup6 = ('mdfe_serie', 'mdfe_numero', 'mdfe_serie_homologacao', 'mdfe_numero_homologacao')
+
+    def to_representation(self, instance):
+        data = {
+            'id': instance.id,
+            'nome': instance.nome,
+        }
+
+        if instance.cnpj:
+            data['cnpj'] = instance.cnpj
         else:
-            tup2 = ('cpf',)
+            data['cpf'] = instance.cpf
 
-        tup3 = ()
-        tup4 = ()
-        tup5 = ()
-        tup6 = ()
+        if instance.nfe_emite:
+            data['nfe_serie'] = instance.nfe_serie
 
-        if model.nfe_emite:
-            tup3 = ('nfe_serie', 'nfe_numero', 'nfe_serie_homologacao', 'nfe_numero_homologacao')
-        if model.nfce_emite:
-            tup4 = ('nfce_serie', 'nfce_numero', 'nfce_serie_homologacao', 'nfce_numero_homologacao')
-        if model.cte_emite:
-            tup5 = ('cte_serie', 'cte_numero', 'cte_serie_homologacao', 'cte_numero_homologacao')
-        if model.mdfe_emite:
-            tup6 = ('mdfe_serie', 'mdfe_numero', 'mdfe_serie_homologacao', 'mdfe_numero_homologacao')
-
-        fields = tup1 + tup2 + tup3 + tup4 + tup5 + tup6
-
-        # print(f'cnpj: {model.cnpj.field()}, \n nfe_emite: {model.nfe_emite.remote_field()}')
+        return data
--- emitentes/api/viewsets.py
+++ emitentes/api/viewsets.py
@@ -1,11 +1,11 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 from emitentes.api import serializers
 from emitentes import models
 
 
 class EmitenteViewSet(viewsets.ModelViewSet):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (AllowAny,)
     serializer_class = serializers.EmitenteSerializer
     queryset = models.Emitente.objects.all()
