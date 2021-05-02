64ad77cf462f6efc32badb617029e5eb03f8faf0
thiago oliveira vicente <thiago.vicente@tivit.com>
2021-05-02 15:47:35 -0300

    refator and update by action

--- emitentes/api/serializers.py
+++ emitentes/api/serializers.py
@@ -1,48 +1,103 @@
 from rest_framework import serializers
-from emitentes import models
+
+from emitentes import models, validators
+
+
+class NFeEmiteSerializer(serializers.ModelSerializer):
+    certificado_a1 = serializers.FileField(validators=[validators.validate_certificado_a1])
+    logotipo_dfe = serializers.FileField(validators=[validators.validate_logotipo_dfe])
+
+    class Meta:
+        model = models.Emitente
+        fields = (
+            'id',
+            'nfe_serie',
+            'logotipo_dfe',
+            'certificado_a1',
+            'nfe_serie_homologacao',
+            'nfe_numero_homologacao',
+        )
+
+
+class NFCeEmiteSerializer(serializers.ModelSerializer):
+    certificado_a1 = serializers.FileField(validators=[validators.validate_certificado_a1])
+    logotipo_dfe = serializers.FileField(validators=[validators.validate_logotipo_dfe])
+
+    class Meta:
+        model = models.Emitente
+        fields = (
+            'id',
+            'nfce_serie',
+            'logotipo_dfe',
+            'certificado_a1',
+            'nfce_serie_homologacao',
+            'nfce_numero_homologacao',
+        )
+
+
+class CTeEmiteSerializer(serializers.ModelSerializer):
+    certificado_a1 = serializers.FileField(validators=[validators.validate_certificado_a1])
+    logotipo_dfe = serializers.FileField(validators=[validators.validate_logotipo_dfe])
+
+    class Meta:
+        model = models.Emitente
+        fields = (
+            'id',
+            'cte_serie',
+            'logotipo_dfe',
+            'certificado_a1',
+            'cte_serie_homologacao',
+            'cte_numero_homologacao',
+        )
+
+
+class MDFeEmiteSerializer(serializers.ModelSerializer):
+    certificado_a1 = serializers.FileField(validators=[validators.validate_certificado_a1])
+    logotipo_dfe = serializers.FileField(validators=[validators.validate_logotipo_dfe])
+
+    class Meta:
+        model = models.Emitente
+        fields = (
+            'id',
+            'mdfe_serie',
+            'logotipo_dfe',
+            'certificado_a1',
+            'mdfe_serie_homologacao',
+            'mdfe_numero_homologacao',
+        )
 
 
 class EmitenteSerializer(serializers.ModelSerializer):
+    certificado_a1 = serializers.FileField(validators=[validators.validate_certificado_a1])
+    logotipo_dfe = serializers.FileField(validators=[validators.validate_logotipo_dfe])
+
     class Meta:
         model = models.Emitente
-        fields = ('id', 'nome', 'certificado_a1', 'logotipo_dfe')
+        fields = (
+            'id',
+            'nome',
+            'nfe_numero',
+            'nfe_serie_homologacao',
+            'certificado_a1',
+            'logotipo_dfe'
+        )
 
     def to_representation(self, instance):
         data = {
             'id': instance.id,
             'nome': instance.nome,
         }
-
         if instance.cnpj:
             data['cnpj'] = instance.cnpj
         else:
             data['cpf'] = instance.cpf
 
-        if instance.nfe_emite:
-            data['nfe_serie'] = instance.nfe_serie
-            data['nfe_numero'] = instance.nfe_numero
-            data['nfe_serie_homologacao'] = instance.nfe_serie_homologacao
-            data['nfe_numero_homologacao'] = instance.nfe_numero_homologacao
-
-        if instance.nfce_emite:
-            data['nfce_serie'] = instance.nfce_serie
-            data['nfce_numero'] = instance.nfce_numero
-            data['nfce_serie_homologacao'] = instance.nfce_serie_homologacao
-            data['nfce_numero_homologacao'] = instance.nfce_numero_homologacao
-
-        if instance.cte_emite:
-            data['cte_serie'] = instance.cte_serie
-            data['cte_numero'] = instance.cte_numero
-            data['cte_serie_homologacao'] = instance.cte_serie_homologacao
-            data['cte_numero_homologacao'] = instance.cte_numero_homologacao
-
-        if instance.mdfe_emite:
-            data['mdfe_serie'] = instance.mdfe_serie
-            data['mdfe_numero'] = instance.mdfe_numero
-            data['mdfe_serie_homologacao'] = instance.mdfe_serie_homologacao
-            data['mdfe_numero_homologacao'] = instance.mdfe_numero_homologacao
-
-        data['certificado_a1'] = instance.certificado_a1.path
-        data['logotipo_dfe'] = instance.logotipo_dfe.path
+        for emit in 'nfe_emite nfce_emite cte_emite mdfe_emite'.split():
+            base_name = emit.split('_')[0]
+            if getattr(instance, emit):
+                data[f'{base_name}_serie'] = getattr(instance, f'{base_name}_serie')
+                data[f'{base_name}_numero'] = getattr(instance, f'{base_name}_numero')
+                data[f'{base_name}_serie_homologacao'] = getattr(instance, f'{base_name}_serie_homologacao')
+                data[f'{base_name}_numero_homologacao'] = getattr(instance, f'{base_name}_numero_homologacao')
 
         return data
